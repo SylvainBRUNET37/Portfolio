3398f229d4ea80258eedde06936e4f871a7245dc
Karo <karo.game37@gmail.com>
2025-12-14 12:46:58 -0500

    feat: ordered favorite games list

--- src/data/games.js
+++ src/data/games.js
@@ -5,20 +5,12 @@ export default
     link: "https://liesofp.com",
   },
   {
-    img: "/images/games/dofus.jpg",
-    link: "https://www.dofus.com",
+    img: "/images/games/dark-souls.jpg",
+    link: "https://en.bandainamcoent.eu/dark-souls/dark-souls-remastered",
   },
   {
-    img: "/images/games/wakfu.jpg",
-    link: "https://www.wakfu.com",
-  },
-  {
-    img: "/images/games/league-of-legends.jpg",
-    link: "https://www.leagueoflegends.com",
-  },
-  {
-    img: "/images/games/minecraft.jpg",
-    link: "https://www.minecraft.net",
+    img: "/images/games/v-rising.jpg",
+    link: "https://playvrising.com",
   },
   {
     img: "/images/games/zelda-twilight-princess.jpg",
@@ -29,20 +21,32 @@ export default
     link: "https://www.pokemon.com/us/pokemon-video-games/pokemon-heartgold-version-and-pokemon-soulsilver-version",
   },
   {
+    img: "/images/games/dofus.jpg",
+    link: "https://www.dofus.com",
+  },
+  {
+    img: "/images/games/wakfu.jpg",
+    link: "https://www.wakfu.com",
+  },
+  {
+    img: "/images/games/civilization.jpg",
+    link: "https://civilization.com/civilization-vi/",
+  },
+  {
     img: "/images/games/might-and-magic.jpg",
     link: "https://www.gog.com/game/heroes_of_might_and_magic_4_complete",
+  },
+  {
+    img: "/images/games/league-of-legends.jpg",
+    link: "https://www.leagueoflegends.com",
   },
   {
     img: "/images/games/rocket-league.jpg",
     link: "https://www.rocketleague.com",
   },
   {
-    img: "/images/games/dark-souls.jpg",
-    link: "https://en.bandainamcoent.eu/dark-souls/dark-souls-remastered",
-  },
-  {
-    img: "/images/games/civilization.jpg",
-    link: "https://civilization.com/civilization-vi/",
+    img: "/images/games/minecraft.jpg",
+    link: "https://www.minecraft.net",
   },
   {
     img: "/images/games/stardew-valley.jpg",
@@ -51,9 +55,5 @@ export default
   {
     img: "/images/games/dont-starve.jpg",
     link: "https://www.klei.com/games/dont-starve",
-  },
-  {
-    img: "/images/games/v-rising.jpg",
-    link: "https://playvrising.com",
   },
 ];
