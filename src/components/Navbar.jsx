bc8379b488637e7aa2226c15d262fb90e3237c92
Karo <sylvain.brunet37@gmail.com>
2025-12-23 15:29:59 -0500

    fix: fixed NavBar URLs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 export default function Navbar() {
+  const base = import.meta.env.BASE_URL;
+
   return (
     <nav className="w-full py-4 border-b">
       <div className="container mx-auto flex items-center justify-between px-4">
-        <a href="/" className="font-bold text-xl">Sylvain Brunet</a>
+        <a href={base} className="font-bold text-xl">
+          Sylvain Brunet
+        </a>
+
         <div className="space-x-4">
-          <a href="/#projects">Main page</a>
-          <a href="/experiences">Experiences</a>
-          <a href="/projects">Projects</a>
-          <a href="/miscellaneous">Miscellaneous</a>
-          <a href="/#contact">Contact</a>
+          <a href={`${base}#projects`}>Main page</a>
+          <a href={`${base}experiences/`}>Experiences</a>
+          <a href={`${base}projects/`}>Projects</a>
+          <a href={`${base}miscellaneous/`}>Miscellaneous</a>
+          <a href={`${base}#contact`}>Contact</a>
         </div>
       </div>
     </nav>
