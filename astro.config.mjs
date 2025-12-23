e8913de94b7328335c22071c34babb5ea8048296
Karo <sylvain.brunet37@gmail.com>
2025-12-23 15:28:14 -0500

    fix: static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://sylvainbrunet37.github.io',
   base: '/Portfolio/',
+  output: 'static',
 
   integrations: [react()],
 
