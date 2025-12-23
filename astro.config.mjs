8a1fba2c02b2bcef2d5430b35d0bca4cf098f31b
Karo <sylvain.brunet37@gmail.com>
2025-12-23 15:18:10 -0500

    feat: add Astro config for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://sylvainbrunet37.github.io',
+  base: '/Portfolio/',
+
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
