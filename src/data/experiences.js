e47ac25e473b916a3d7bb74612a7b73c283e0449
Karo <sylvain.brunet37@gmail.com>
2025-12-23 22:12:08 -0500

    feat: updated the description of my experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -6,9 +6,9 @@ export default
     period: "April 2025 - August 2025",
     summary: "Development of communication gateways between industrial equipment and factory control systems",
     description: [
-        "I participated in a team project aimed at making an Agileo framework usable across multiple programming languages.",
-        "I regularly delivered C# features within a client-server architecture, including unit test implementation and participation in code reviews.",
-        "By the end of the internship, I also developed a prototype of an IoT gateway using OPC-UA.",
+        "I participated in a project aimed at making an Agileo framework usable in several programming languages.",
+        "I regularly delivered C# features within a client–server architecture, including unit test implementation and a code review process.",
+        "At the end of the internship, I also developed a prototype for another type of gateway.",
     ],
     tech: [
       "C#",
@@ -34,8 +34,8 @@ export default
     period: "May 2022 - July 2022",
     summary: "Implementation of a database and development of a calculation tool for data processing in C++",
     description: [
-      "I performed requirements analysis, software architecture design, database modeling, and automation of data ingestion from spreadsheet files.",
-      "I also designed and implemented a database and developed a calculation tool to support data analysis."
+      "I carried out the requirements analysis, architectural design, and development of software that automates the extraction of data from spreadsheet files.",
+      "I also designed and implemented a database to store this data and developed a calculation tool to facilitate its analysis."
     ],
     tech: [
         "Needs Analysis",
